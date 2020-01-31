331a49b9f251e30c4ffec3585d8845b0678aad27
sk97045 <sk97045@gmail.com>
2020-01-31 09:28:57 +0530

    Module Application startup and configuration

--- OdeToFood/OdeToFood.Web/Controllers/HomeController.cs
+++ OdeToFood/OdeToFood.Web/Controllers/HomeController.cs
@@ -11,9 +11,10 @@ namespace OdeToFood.Web.Controllers
     {
         IRestaurantData db;
 
-        public HomeController()
+        public HomeController(IRestaurantData db)
         {
-            db = new InMemoryRestaurantData();
+            //db = new InMemoryRestaurantData();
+            this.db = db;        
         }
 
         public ActionResult Index()
